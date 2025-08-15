9d4a9e08ced5c8191a5868dad8a517f253b60ed6
Jonathan Perkins <jonperk318@gmail.com>
2025-08-15 09:12:04 -0400

    Added theme toggle and logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,8 +6,10 @@ import {
   AkInfo,
   AkPeopleGroup,
   AkPerson,
+  BxSad,
 } from "@kalimahapps/vue-icons";
 import { RouterLink } from "vue-router";
+import { BsMoonFill, BsSunFill } from "@kalimahapps/vue-icons";
 
 const navigation = [
   { name: "Home", to: "/", icon: AkHomeAlt1 },
@@ -17,18 +19,24 @@ const navigation = [
   { name: "Friends", to: "/friends", icon: AkPeopleGroup },
   { name: "Profile", to: "/profile", icon: AkPerson },
 ];
+
+const { t } = useI18n();
 </script>
 
 <template>
   <nav class="w-screen h-18 flex items-center px-8 bg-base-100 text-lg">
-    <div class="w-1/2 h-full flex justify-start items-center">
-      <div>LOGO</div>
+    <div class="w-1/2 h-full flex gap-8 justify-start items-center">
+      <img class="w-24" src="/assets/logo.png" />
+      <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
+        <BsMoonFill v-if="isDark" />
+        <BsSunFill v-else />
+      </button>
     </div>
     <div
       class="h-2/3 flex justify-end items-center rounded-box gap-2 border-4 border-neutral"
     >
       <div class="group" v-for="nav in navigation" :key="nav.name">
-        <RouterLink class="btn btn-ghost group-hover:btn-primary" to="nav.to">
+        <RouterLink class="btn btn-ghost group-hover:btn-primary" :to="nav.to">
           <component :is="nav.icon" class="mr-2 text-2xl" />
           <span class="opacity-0 group-hover:opacity-100">{{ nav.name }}</span>
         </RouterLink>
