c9b58005b994d1a00d679d9499c8d36d6962c612
Jonathan Perkins <jonperk318@gmail.com>
2025-09-18 08:48:14 -0400

    Rearranged navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,6 @@ import {
   AkInfo,
   AkPeopleGroup,
   AkPerson,
-  BxSad,
 } from "@kalimahapps/vue-icons";
 import { RouterLink } from "vue-router";
 import { BsMoonFill, BsSunFill } from "@kalimahapps/vue-icons";
@@ -25,20 +24,23 @@ const { t } = useI18n();
 
 <template>
   <nav class="w-screen h-18 flex items-center px-8 bg-base-100 text-lg">
-    <div class="w-1/2 h-full flex gap-8 justify-start items-center">
+    <div class="w-1/2 h-full flex justify-start items-center">
       <img class="w-24" src="/assets/logo.png" />
-      <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
+    </div>
+    <div class="w-1/2 flex justify-end items-center rounded-box gap-2">
+      <button
+        icon-btn
+        :title="t('button.toggle_dark')"
+        @click="toggleDark()"
+        class="cursor-pointer mr-2"
+      >
         <BsMoonFill v-if="isDark" />
         <BsSunFill v-else />
       </button>
-    </div>
-    <div
-      class="h-2/3 flex justify-end items-center rounded-box gap-2 border-4 border-neutral"
-    >
       <div class="group" v-for="nav in navigation" :key="nav.name">
         <RouterLink class="btn btn-ghost group-hover:btn-primary" :to="nav.to">
-          <component :is="nav.icon" class="mr-2 text-2xl" />
-          <span class="opacity-0 group-hover:opacity-100">{{ nav.name }}</span>
+          <component :is="nav.icon" class="group-hover:mr-2 text-2xl" />
+          <span class="hidden group-hover:block">{{ nav.name }}</span>
         </RouterLink>
       </div>
     </div>
